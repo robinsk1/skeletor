b3f2ff06b45d142794fb57af0f693e969df91157
robin <robinskilling@gmail.com>
2013-05-02 12:37:17 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skeletor::Application.config.secret_token = 'd379402598a1192249fccef6ee1bd4448fff3bf9f8177e2de48cca9b60386bae65d57e17197e9bc92a51ba8796aab24b2d1a832e112f9fc1934d05808671e693'
