1653e94f5588833d04f9bf5f55087c9a23f77acf
robinsk1 <robinskilling@gmail.com>
2013-10-05 15:03:05 +0200

    minor fixes

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,6 +2,5 @@ class Picture < ActiveRecord::Base
   attr_accessible :name, :cover_image_name, :cover_image_url, :cover_image
   image_accessor :cover_image
   belongs_to :imageable, :polymorphic => true
-
   resourcify
 end
